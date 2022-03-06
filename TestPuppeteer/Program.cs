f05fce07e3b29049295cf80363f2d14977fca285
Pablo Castells <pablo.ezequiel.castells@gmail.com>
2022-03-06 18:59:17 -0300

    Add project files.

---
+++ TestPuppeteer/Program.cs
@@ -0,0 +1,19 @@
+﻿using PuppeteerSharp;
+
+string outPut = Path.Combine(Environment.CurrentDirectory, "Pdfs", "test.pdf");
+using var browserFetcher = new BrowserFetcher();
+await browserFetcher.DownloadAsync();
+
+await using var browser = await Puppeteer.LaunchAsync(
+    new LaunchOptions()
+    {
+        Headless = true,
+    });
+
+await using var page = await browser.NewPageAsync();
+
+// await page.GoToAsync("https://www.google.com");
+
+await page.SetContentAsync("<div> test </div>");
+await page.PdfAsync(outPut);
+
